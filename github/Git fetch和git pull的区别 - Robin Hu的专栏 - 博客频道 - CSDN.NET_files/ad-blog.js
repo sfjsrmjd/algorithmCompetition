df9d2d537b8c9a3fc4f10fe41a41d19e62bd07a3
heweiwei <kusee8@qq.com>
2014-04-29 06:44:34 +0800

    我把在网上看到的有用的git使用资料全部放在github文件夹中，便于以后查看。

---
+++ github/Git fetch和git pull的区别 - Robin Hu的专栏 - 博客频道 - CSDN.NET_files/ad-blog.js
@@ -0,0 +1,30 @@
+﻿var ad_type = 'ms752';
+var ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+
+
+ ad_type = 'ms755_2';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+/**/
+
+ ad_type = 'ms757_3';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+/*
+ ad_type = 'ms731';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+*/
