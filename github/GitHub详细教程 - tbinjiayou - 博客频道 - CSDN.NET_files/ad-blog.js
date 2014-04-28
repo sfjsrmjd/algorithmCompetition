38303feeb570e1bfd750c642991d5ca5d666ec0e
heweiwei <kusee8@qq.com>
2014-04-29 06:40:00 +0800

    我需要把本地的文件与远端的文件合并。

---
+++ github/GitHub详细教程 - tbinjiayou - 博客频道 - CSDN.NET_files/ad-blog.js
@@ -0,0 +1,30 @@
+﻿var ad_type = 'ms746';
+var ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+/*document.writeln(ad_sc);
+
+
+ ad_type = 'ms748_2';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+*/
+/*
+ ad_type = 'ms716';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+
+ ad_type = 'ms731';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+*/
