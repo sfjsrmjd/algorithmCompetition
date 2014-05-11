6c16620e32b31c177134a6752a3cc2fccb29fed1
heweiwei <kusee8@qq.com>
2014-05-11 18:52:52 +0800

    今天有一个新想法就是每天学习一点vim，对我的日常编程作用很大！

--- 第一部分语言篇/第一章程序设计入门/第二小节变量及其输入/A+B问题/a+b.c
+++ 第一部分语言篇/第一章程序设计入门/第二小节变量及其输入/A+B问题/a+b.c
@@ -5,7 +5,10 @@ main (void)
   int a, b;
 //there need int * don't need int!
   //scanf ("%d%d", a, b);
+  printf("请输入两个整数，我们将为你计算你所给出的两个整数的和。\n");
   scanf ("%d%d", &a, &b);
+  printf("%d加%d的和是",a,b);
   printf ("%d\n", a + b);
+  printf("谢谢使用，拜拜\n");
   return 0;
 }
