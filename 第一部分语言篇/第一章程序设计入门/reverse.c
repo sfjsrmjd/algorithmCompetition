07e34a340bfc645ba3fb7cdce67cdf11844379b5
heweiwei <kusee8@qq.com>
2014-05-18 13:27:14 +0800

    我在每天积极推进算法的学习这是我的喜欢，就像我打羽毛球一样，只要按自己的想法去打，失误是可以接受的，如果乱打即使赢了，我也觉得没意思，这里程序怎么写，写成什么样子也按自己的想法来就是了！

--- 第一部分语言篇/第一章程序设计入门/reverse.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int
-main (void)
-{
-  int n;
-  scanf ("%d", &n);
-  printf ("%d%d%d\n", n % 10, n / 10 % 10, n / 100);
-  return 0;
-}
--- 第一部分语言篇/第一章程序设计入门/第二小节变量及其输入/圆柱体的表面积/cylinder_surface_area.c
+++ 第一部分语言篇/第一章程序设计入门/第二小节变量及其输入/圆柱体的表面积/cylinder_surface_area.c
@@ -5,11 +5,21 @@ main (void)
 {
   const double pi = 4.0 * atan (1.0);
   double r, h, s1, s2, s;
+  printf("请输入圆柱体的底面半径和高： ");
   scanf ("%lf%lf", &r, &h);
-  //pi * r * r = s1;
   s1 = pi * r * r;
   s2 = 2 * pi * r * h;
   s = s1 * 2.0 + s2;
-  printf ("Area = %.3lf\n", s);
+  printf ("该圆柱体的表面积是%.3lf\n", s);
+
+  s1 = M_PI * r * r;
+  s2 = 2 * M_PI * r * h;
+  s = s1 * 2.0 + s2;
+  printf ("该圆柱体的表面积是%.3lf\n", s);
+
+  printf ("我想看一下两种圆周率的差别：\n");
+  printf("由M_PI给出的圆周率是：%lf\n",M_PI);
+  printf("由4.0*atan(1.0)给出的圆周率是：%lf\n",pi);
+  //测试结果是两者给出的圆周率是相同的！
   return 0;
 }
