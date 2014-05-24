96343258962a7e50ae95a63ed87af446bfdd7517
heweiwei <kusee8@qq.com>
2014-05-24 08:47:49 +0800

    坚持每天的学习真的不容易，每天都有很多困扰烦恼，可是真正喜欢的东西总会带给你好的一切，所以加油坚持！

--- 第一部分语言篇/第一章程序设计入门/第三小节顺序结构程序设计/交换变量/交换变量直接法/exchange_variables.c
+++ 第一部分语言篇/第一章程序设计入门/第三小节顺序结构程序设计/交换变量/交换变量直接法/exchange_variables.c
@@ -3,7 +3,9 @@ int
 main (void)
 {
   int a, b;
+  printf("请输入两个整数：");
   scanf ("%d%d", &a, &b);
+  printf("两个整数交换位置后的输出是：");
   printf ("%d %d\n", b, a);
   return 0;
 }
--- 第一部分语言篇/第一章程序设计入门/第四小节分支结构程序设计/三整数排序/穷举逐一测试法/three_integer_sorting.c
+++ 第一部分语言篇/第一章程序设计入门/第四小节分支结构程序设计/三整数排序/穷举逐一测试法/three_integer_sorting.c
@@ -1,3 +1,4 @@
+/*  三整数排序：输入三个整数，从小到大排序后输出。 */
 #include<stdio.h>
 
 int
@@ -6,24 +7,26 @@ main ()
 
   int a, b, c;
 
+  printf ("请输入三个整数（我们将为它们排序）：");
   scanf ("%d%d%d", &a, &b, &c);
 
+  printf ("排序后的三个整数是：");
   if (a <= b && b <= c)
     printf ("%d %d %d\n", a, b, c);
 
-  if (a <= c && c <= b)
+  else if (a <= c && c <= b)
     printf ("%d %d %d\n", a, c, b);
 
-  if (b <= a && a <= c)
+  else if (b <= a && a <= c)
     printf ("%d %d %d\n", b, a, c);
 
-  if (b <= c && c <= a)
+  else if (b <= c && c <= a)
     printf ("%d %d %d\n", b, c, a);
 
-  if (c <= a && a <= b)
+  else if (c <= a && a <= b)
     printf ("%d %d %d\n", c, a, b);
 
-  if (c <= b && b <= a)
+  else if (c <= b && b <= a)
     printf ("%d %d %d\n", c, b, a);
 
   return 0;
