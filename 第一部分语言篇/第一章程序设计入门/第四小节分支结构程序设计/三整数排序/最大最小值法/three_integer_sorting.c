0d9ac9095cc5e29663baa1aecf1698bc1b0ca148
heweiwei <kusee8@qq.com>
2014-05-26 06:40:34 +0800

    我需要寻找自信，让自己强大起来，就像迎接编程中的挑战一样，迎接生活中的挑战！

--- 第一部分语言篇/第一章程序设计入门/第四小节分支结构程序设计/三整数排序/最大最小值法/three_integer_sorting.c
+++ 第一部分语言篇/第一章程序设计入门/第四小节分支结构程序设计/三整数排序/最大最小值法/three_integer_sorting.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main(){
   int a, b, c, x, y, z;
+  printf("请输入三个整数（我们将为你从小到大排序它们）:");
   scanf("%d%d%d", &a, &b, &c);
   //x = a <? b <? c;
   //find min
@@ -9,6 +10,7 @@ int main(){
   z = a; if(b > z) z = b; if(c > z) z = c;
   //compute middle
   y = a + b + c - x - z;
+  printf("我们的排序结果是：");
   printf("%d %d %d\n", x, y, z);
   return 0;
 }
